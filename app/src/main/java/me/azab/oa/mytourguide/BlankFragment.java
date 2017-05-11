3e48d67927f56bcc47e54e167904d29112805db9
Omar <oa.azab@gmail.com>
2017-05-11 14:01:56 +0200

    About Fragment

--- app/src/main/java/me/azab/oa/mytourguide/BlankFragment.java
+++ app/src/main/java/me/azab/oa/mytourguide/BlankFragment.java
@@ -3,9 +3,11 @@ package me.azab.oa.mytourguide;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
@@ -13,16 +15,18 @@ import android.view.ViewGroup;
  */
 public class BlankFragment extends Fragment {
 
+    TextView mTextView;
 
     public BlankFragment() {
         // Required empty public constructor
     }
 
-
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_blank, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_blank, container, false);
+        mTextView = (TextView) rootView.findViewById(R.id.main_text_about);
+        mTextView.setText(Html.fromHtml(getString(R.string.about_text)));
+        return rootView;
     }
 
 }
--- app/src/main/java/me/azab/oa/mytourguide/MainActivity.java
+++ app/src/main/java/me/azab/oa/mytourguide/MainActivity.java
@@ -1,13 +1,30 @@
 package me.azab.oa.mytourguide;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    FrameLayout mFrameLayout;
+    FragmentManager mFragmentManager;
+    FragmentTransaction mFragmentTransaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Find UI
+        mFrameLayout = (FrameLayout) findViewById(R.id.main_container);
+
+        // Add Fragment to framelayout
+        mFragmentManager = getSupportFragmentManager();
+        mFragmentTransaction = mFragmentManager.beginTransaction();
+        BlankFragment mFragment = new BlankFragment();
+        mFragmentTransaction.add(R.id.main_container,mFragment);
+        mFragmentTransaction.commit();
     }
 }
